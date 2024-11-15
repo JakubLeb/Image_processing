d7550765c574ef586399f27cd8dc7df0a646d595
nazwi <jaklebiodzinski@gmail.com>
2024-11-15 16:55:13 +0100

    zad  complete

--- lab5/lab5_5.py
+++ lab5/lab5_5.py
@@ -1,19 +1,31 @@
 import cv2
 import numpy as np
 
+def sharpen_gaussian(img):
+    sharpened = cv2.GaussianBlur(img, (0, 0), 3)
+    sharpened = cv2.addWeighted(img, 1.5, sharpened, -0.5, 0)
+    return sharpened
+
+def sharpen_laplacian(img):
+    laplacian = cv2.Laplacian(img, cv2.CV_64F)
+    sharpened = img + laplacian
+    return sharpened.astype(np.uint8)
+
+def sharpen_custom(img):
+    kernel = np.array([[-1, -1, -1],
+                      [-1, 9, -1],
+                      [-1, -1, -1]])
+    sharpened = cv2.filter2D(img, -1, kernel)
+    return sharpened
+
 img = cv2.imread("gang.jpg")
-img = cv2.resize(img, (300,300))
+img = cv2.resize(img, (300, 300))
 
-kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-kernelx2 = kernel * 2
-kernelx5 =  kernel * 5
-im1 = cv2.filter2D(img, -1, kernel)
-im2 = cv2.filter2D(img, -1, kernelx2)
-im3 = cv2.filter2D(img, -1, kernelx5)
+sharpened_gaussian = sharpen_gaussian(img.copy())
+sharpened_laplacian = sharpen_laplacian(img.copy())
+sharpened_custom = sharpen_custom(img.copy())
 
-combine = np.hstack((img , im1, im2, im3))
-cv2.imshow("Obrazy z filtrem median", combine)
-
+combine = np.hstack((img, sharpened_gaussian, sharpened_laplacian, sharpened_custom))
+cv2.imshow("Image Sharpening Techniques", combine)
 cv2.waitKey(0)
-
 cv2.destroyAllWindows()
